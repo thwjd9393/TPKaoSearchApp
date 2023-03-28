233940319a051a99387b1966e98c222d2c8b0e9d
thwjd9393 <thwjd9393@naver.com>
2023-03-28 10:46:04 +0900

    splashActivity Launcher로 등록

---
+++ app/src/main/java/com/bsj0420/tpkaosearchapp/activities/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.bsj0420.tpkaosearchapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bsj0420.tpkaosearchapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
