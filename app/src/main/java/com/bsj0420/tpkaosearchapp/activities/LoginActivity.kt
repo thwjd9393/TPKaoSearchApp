1ccb3b17349439e184a22540fa7897f233c903c7
thwjd9393 <thwjd9393@naver.com>
2023-03-29 10:58:39 +0900

    login UI 버튼 리스너 처리

--- app/src/main/java/com/bsj0420/tpkaosearchapp/activities/LoginActivity.kt
+++ app/src/main/java/com/bsj0420/tpkaosearchapp/activities/LoginActivity.kt
@@ -1,12 +1,51 @@
 package com.bsj0420.tpkaosearchapp.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bsj0420.tpkaosearchapp.R
+import com.bsj0420.tpkaosearchapp.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    val binding:ActivityLoginBinding by lazy { ActivityLoginBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+
+        //둘러보기 버튼으로 로그인 없이 메인화면으로 이동
+        binding.tvGo.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }
+
+        //회원가입 버튼 클릭 했을 떄 반응
+        binding.tvSingup.setOnClickListener {
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
+
+        //이메일 로그인 버튼 클릭
+        binding.layoutEmail.setOnClickListener {
+            startActivity(Intent(this,EmailSigninActivity::class.java))
+        }
+        
+        //간편로그인 버튼
+        binding.ivLoginKakao.setOnClickListener { clickLoginKakao() }
+        binding.ivLoginGoogle.setOnClickListener { clickLoginGoogle() }
+        binding.ivLoginNaver.setOnClickListener { clickLoginNaver() }
+
+    }// onCreate
+
+    private fun clickLoginKakao() {
+        TODO("Not yet implemented")
+    }
+
+    private fun clickLoginGoogle() {
+        TODO("Not yet implemented")
+    }
+
+    private fun clickLoginNaver() {
+        TODO("Not yet implemented")
     }
 }
